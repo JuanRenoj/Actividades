16ae6c9765aa8107d504db438b0fbdb6057aa7fd
Juan Renoj <jrenoj@gmail.com>
2024-06-05 22:35:17 -0600

    first commit

---
+++ src/pages/activdad/Activdad.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Activdad() {
+  return (
+    <div>
+      Activdad
+    </div>
+  )
+}
+
+export default Activdad
---
+++ src/service/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiTmgu6Hw4ncfAXYqzmbxpt7_Z1ZET19s",
+  authDomain: "actividad-e5368.firebaseapp.com",
+  projectId: "actividad-e5368",
+  storageBucket: "actividad-e5368.appspot.com",
+  messagingSenderId: "881958514837",
+  appId: "1:881958514837:web:e485c52ee80fa5cfdfeb9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
